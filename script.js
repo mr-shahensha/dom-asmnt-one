a52b0fa8b4bab106d9ccf66a3334620edcb20f30
shahensha <64788000+mr-shahensha@users.noreply.github.com>
2023-05-14 23:25:55 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let decbtn=document.getElementById("decrement-btn");
-let disval=document.getElementById("display-value");
-decbtn.addEventListener("click",()=>{
-    const value = Number(disval.innerText);
-    if (value > 0) {
-        disval.innerText = value - 1;
-    } else {
-      alert("Negative value not allowed");
-    }
-});
-console.log(decbtn);
+const counterValue = document.getElementById('counter');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const resetButton = document.getElementById('reset');
 
+let counter = 0;
+
+function updateCounter() {
+    counterValue.innerText = counter;
+}
+
+function incrementCounter() {
+    counter += 1;
+    updateCounter();
+}
+
+function decrementCounter() {
+    counter -= 1;
+    if(counter<=-1){
+      alert("value can not be negative");
+    }else{
+      updateCounter();
+    }
+}
+
+function resetCounter() {
+    counter = 0;
+    updateCounter();
+}
+
+incrementButton.addEventListener('click', incrementCounter);
+decrementButton.addEventListener('click', decrementCounter);
+resetButton.addEventListener('click', resetCounter);
